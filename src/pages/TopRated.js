5bbbea247aba134c0f188e7d062f00f123dd7391
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-27 19:29:04 +0600

    get top rating product

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,9 +1,35 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProduct } from "../context/ProductProvider";
 
 const TopRated = () => {
+  const {
+    state: { products, loading, error },
+  } = useProduct();
+
+  let content;
+
+  if (loading) {
+    content = <p>Loading</p>;
+  }
+
+  if (error) {
+    content = <p>Something went wrong!!</p>;
+  }
+
+  if (!loading && !error && products.length === 0) {
+    content = <p>Nothing to show. Product list is empty.</p>;
+  }
+
+  if (!loading && !error && products.length) {
+    content = products
+      .filter((product) => product.rating >= 4)
+      .map((product) => <ProductCard key={product._id} product={product} />);
+  }
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is featured page</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {content}
     </div>
   );
 };

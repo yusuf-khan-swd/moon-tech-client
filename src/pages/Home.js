73135681bd385e74bca5db9b1c434f571e6c3e0a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-27 19:15:12 +0600

    call custom hook for products and map upon it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
 import { useProduct } from "../context/ProductProvider";
 
 const Home = () => {
-  const { data } = useProduct();
+  const {
+    state: { products },
+  } = useProduct();
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
   );
 };

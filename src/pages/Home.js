0ed2807e1c0105f18687af1edf8f42970e88b396
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-27 19:27:40 +0600

    handle loading and error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,32 @@ import { useProduct } from "../context/ProductProvider";
 
 const Home = () => {
   const {
-    state: { products },
+    state: { products, loading, error },
   } = useProduct();
+
+  let content;
+
+  if (loading) {
+    content = <p>Loading</p>;
+  }
+
+  if (error) {
+    content = <p>Something went wrong!!</p>;
+  }
+
+  if (!loading && !error && products.length === 0) {
+    content = <p>Nothing to show. Product list is empty.</p>;
+  }
+
+  if (!loading && !error && products.length) {
+    content = products.map((product) => (
+      <ProductCard key={product._id} product={product} />
+    ));
+  }
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      {products.map((product) => (
-        <ProductCard key={product._id} product={product} />
-      ))}
+      {content}
     </div>
   );
 };

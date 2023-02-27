739850fa60c272172e66fad8728dd0208cd4233b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-27 18:26:56 +0600

    create a context to share product from fetch to entire application

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useProduct } from "../context/ProductProvider";
 
 const Home = () => {
+  const { data } = useProduct();
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       <h1>This is home page</h1>
